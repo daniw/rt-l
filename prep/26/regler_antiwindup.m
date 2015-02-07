4eafe46c716cee6700d15a519287ba4b81571d54
daniw <dani.winz@bluewin.ch>
2015-02-07 15:08:26 +0100

    26

---
+++ prep/26/regler_antiwindup.m
@@ -0,0 +1,36 @@
+K = 0.05;
+R = 0.5;
+J = 0.00025;
+a = 0.001;
+
+tau = J*R / (K*K + a*R);
+Kg = K / (K*K + a*R);
+
+mmax = 0.00;
+g_antiwindup = 1;
+
+Kp = 0.1;
+Ti = tau;
+sim('regler_antiwindup_mdl.slx');
+subplot(2,2,1);
+plot(omega.time, omega.signals.values);
+
+Kp = 0.2;
+Ti = tau;
+sim('regler_antiwindup_mdl.slx');
+subplot(2,2,2);
+plot(omega.time, omega.signals.values);
+
+Kp = 0.5;
+Ti = tau;
+sim('regler_antiwindup_mdl.slx');
+subplot(2,2,3);
+plot(omega.time, omega.signals.values);
+
+Kp = 1;
+Ti = tau;
+sim('regler_antiwindup_mdl.slx');
+subplot(2,2,4);
+plot(omega.time, omega.signals.values);
+
+print '-dpdf' 'regler_antiwindup_plot.pdf';
