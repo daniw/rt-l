5b1bded6805ca863c08d46c4a03020c21856367d
daniw <dani.winz@bluewin.ch>
2015-02-07 13:38:03 +0100

    22

---
+++ prep/22/regler_pi.m
@@ -0,0 +1,17 @@
+K = 0.05;
+R = 0.5;
+J = 0.00025;
+a = 0.001;
+
+tau = J*R / (K*K + a*R);
+Kg = K / (K*K + a*R);
+
+mmax = 0.00;
+
+Kp = 10
+Ti = tau
+
+sim('regler_pi_mdl.slx');
+plot(omega.time, omega.signals.values);
+
+print '-dpdf' 'regler_pi_plot.pdf';
