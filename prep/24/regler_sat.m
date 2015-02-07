0b9ea9c6321a88de3590a6ff3cefd02aea5d833c
daniw <dani.winz@bluewin.ch>
2015-02-07 14:05:42 +0100

    24

---
+++ prep/24/regler_sat.m
@@ -0,0 +1,35 @@
+K = 0.05;
+R = 0.5;
+J = 0.00025;
+a = 0.001;
+
+tau = J*R / (K*K + a*R);
+Kg = K / (K*K + a*R);
+
+mmax = 0.00;
+
+Kp = 0.1;
+Ti = tau;
+sim('regler_sat_mdl.slx');
+subplot(2,2,1);
+plot(omega.time, omega.signals.values);
+
+Kp = 0.2;
+Ti = tau;
+sim('regler_sat_mdl.slx');
+subplot(2,2,2);
+plot(omega.time, omega.signals.values);
+
+Kp = 0.5;
+Ti = tau;
+sim('regler_sat_mdl.slx');
+subplot(2,2,3);
+plot(omega.time, omega.signals.values);
+
+Kp = 1;
+Ti = tau;
+sim('regler_sat_mdl.slx');
+subplot(2,2,4);
+plot(omega.time, omega.signals.values);
+
+print '-dpdf' 'regler_sat_plot.pdf';
