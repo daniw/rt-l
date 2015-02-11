6886c8a7cda4bc233428db462ee4e6877b646a5b
daniw <dani.winz@bluewin.ch>
2015-02-11 16:48:37 +0100

    Regler mit siso

---
+++ drehzahl/10/pi_siso.m
@@ -0,0 +1,22 @@
+s = tf('s');
+
+% Übertragungsfunktionen
+t1_noload_man   = 0.1727*0.85;
+t1_load_man     = 0.1269*0.8;
+
+td_noload_man   = 0.035;    % Keine Totzeit
+td_load_man     = 0.045;    % Keine Totzeit
+
+Kg_noload_man   = 245.5;
+Kg_load_man     = 150.5;
+
+G11_man = Kg_noload_man / (t1_noload_man * s + 1)^2 * exp(-td_noload_man*s);
+G12_man = Kg_load_man / (t1_load_man * s + 1)^2 * exp(-td_load_man*s);
+
+G_Arr(:,:,1) = G11_man;
+G_Arr(:,:,2) = G12_man;
+
+% sisotool
+%sisotool(G_Arr);
+C_PI = 0.0040628 * (s + 5.23) / s;
+C_PID = 0.004925 * (s + 10.79) * (s + 5.337) / (s * (s + 12.5));
