65a20f4f22be0f22621895fb08182f38ccb049c5
Nino Ninux <nino.ninux@gmail.com>
2015-02-12 16:47:37 +0100

    Plots erstellt für Aufgaben 3, 4, 6

---
+++ luftkanal/06/pdfcrop.sh
@@ -0,0 +1,4 @@
+pdfcrop p_krit_full_plot.pdf p_krit_full_plot.pdf 
+pdfcrop p_krit_half_plot.pdf p_krit_half_plot.pdf
+pdfcrop pid_zn_full_plot.pdf pid_zn_full_plot.pdf
+pdfcrop pid_zn_half_plot.pdf pid_zn_half_plot.pdf
