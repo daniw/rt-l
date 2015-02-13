a2b931216286de105655d63e6f11db0b508b4b7a
Nino Ninux <nino.ninux@gmail.com>
2015-02-13 10:23:17 +0100

    Niveau - Aufgabe 11 dokumentiert (Simulation SISO-Regler)

---
+++ niveau/11/plots.m
@@ -0,0 +1,93 @@
+% run the system and simulation configuration
+run('regler_siso');
+
+% simulate
+sim('model');
+
+% plot overview for 'L5'
+figure(1);
+subplot(2,1,1);
+plot( ...
+    h5.time, h5.signals.values, ... % Füllstand
+    r5.time, r5.signals.values);    % Sollstand
+legend('Füllstand [cm]', 'Sollstand [cm]');
+xlabel('Zeit [t]');
+ylabel('Füllstand [cm]');
+title('Sprungantwort - Übersicht');
+axis([495 515 2.75 3.75]);
+
+subplot(2,1,2);
+plot( ...
+    u5.time, u5.signals.values);    % Pumpe
+legend('Pumpenspannung [V]');
+xlabel('Zeit [t]');
+ylabel('Pumpenspannung [V]');
+axis([495 515 0 10]);
+
+print -dpdf 'L5_step_overview_plot'
+
+% plot overshoot for 'L5'
+figure(2);
+subplot(2,1,1);
+plot( ...
+    h5.time, h5.signals.values, ... % Füllstand
+    r5.time, r5.signals.values);    % Sollstand
+legend('Füllstand [cm]', 'Sollstand [cm]');
+xlabel('Zeit [t]');
+ylabel('Füllstand [cm]');
+title('Sprungantwort - Überschwingen');
+axis([490 690 3.495 3.505]);
+
+subplot(2,1,2);
+plot( ...
+    u5.time, u5.signals.values);    % Pumpe
+legend('Pumpenspannung [V]');
+xlabel('Zeit [t]');
+ylabel('Pumpenspannung [V]');
+axis([490 690 0 10]);
+
+print -dpdf 'L5_step_overshoot_plot'
+
+% plot overview for 'L7'
+figure(3);
+subplot(2,1,1);
+plot( ...
+    h7.time, h7.signals.values, ... % Füllstand
+    r7.time, r7.signals.values);    % Sollstand
+legend('Füllstand [cm]', 'Sollstand [cm]');
+xlabel('Zeit [t]');
+ylabel('Füllstand [cm]');
+title('Sprungantwort - Übersicht');
+axis([495 515 11.75 12.75]);
+
+subplot(2,1,2);
+plot( ...
+    u7.time, u7.signals.values);    % Pumpe
+legend('Pumpenspannung [V]');
+xlabel('Zeit [t]');
+ylabel('Pumpenspannung [V]');
+axis([495 515 -1 11]);
+
+print -dpdf 'L7_step_overview_plot'
+
+% plot overshoot for 'L5'
+figure(4);
+subplot(2,1,1);
+plot( ...
+    h7.time, h7.signals.values, ... % Füllstand
+    r7.time, r7.signals.values);    % Sollstand
+legend('Füllstand [cm]', 'Sollstand [cm]');
+xlabel('Zeit [t]');
+ylabel('Füllstand [cm]');
+title('Sprungantwort - Überschwingen');
+axis([490 690 12.45 12.55]);
+
+subplot(2,1,2);
+plot( ...
+    u7.time, u7.signals.values);    % Pumpe
+legend('Pumpenspannung [V]');
+xlabel('Zeit [t]');
+ylabel('Pumpenspannung [V]');
+axis([490 690 -1 11]);
+
+print -dpdf 'L7_step_overshoot_plot'
