64ce122cdc44002a230d20ea5e2284f26f7acdfd
Nino Ninux <nino.ninux@gmail.com>
2015-02-13 16:31:39 +0100

    Plots verbessert (D,9)

--- drehzahl/09/plots.m
+++ drehzahl/09/plots.m
@@ -5,37 +5,41 @@ data_n = load('dc_step_20_25_05');
 % plot step with no load
 figure(1);
 subplot(2,1,1);
-plot(data_n.Zeit, data_n.Geschwindigkeit);
-legend('Geschwindigkeit [rpm]');
+plot( ...
+    data_n.Zeit, data_n.Geschwindigkeit, ...
+    data_n.Zeit, data_n.Sollgeschwindigkeit);
+legend('Geschwindigkeit [rpm]', 'Sollgeschwindigkeit [rpm]');
 xlabel('Zeit [s]');
 ylabel('Geschwindigkeit [rpm]');
 title('Schrittantwort ohne Bremse');
-ylim([1900 2700]);
+axis([4 8 1900 2700]);
 
 subplot(2,1,2);
 plot(data_n.Zeit, data_n.Motorspannung);
 legend('Motorspannung [V]');
 xlabel('Zeit [s]');
 ylabel('Motorspannung [V]');
-ylim([5 30]);
+axis([4 8 5 30]);
 
 print -dpdf 'step_noload'
 
 % plot step with load
 figure(2);
 subplot(2,1,1);
-plot(data_l.Zeit, data_l.Geschwindigkeit);
-legend('Geschwindigkeit [rpm]');
+plot( ...
+    data_l.Zeit, data_l.Geschwindigkeit, ...
+    data_l.Zeit, data_l.Sollgeschwindigkeit);
+legend('Geschwindigkeit [rpm]', 'Sollgeschwindigkeit [rpm]');
 xlabel('Zeit [s]');
 ylabel('Geschwindigkeit [rpm]');
 title('Schrittantwort mit Bremse');
-ylim([1900 2700]);
+axis([4 8 1900 2700]);
 
 subplot(2,1,2);
 plot(data_l.Zeit, data_l.Motorspannung);
 legend('Motorspannung [V]');
 xlabel('Zeit [s]');
 ylabel('Motorspannung [V]');
-ylim([5 30]);
+axis([4 8 5 30]);
 
 print -dpdf 'step_load'
